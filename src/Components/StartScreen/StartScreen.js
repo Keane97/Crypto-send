68fe922bc4849a646fea3c41173fe1a36db8ecba
Keane97 <liamkeane49@yahoo.com>
2021-06-30 18:33:00 +0100

    StartScreen
    Created the UI for the StartScreen component.
    Tested on Mobile device via localhost connect.

--- src/Components/StartScreen/StartScreen.js
+++ src/Components/StartScreen/StartScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import "./StartScreen.css";
+
+
+// import CryptoSendLogo from "../../CryptoSendLogo.png";
 
 class StartScreenComponent extends React.Component{
 
 
     render() {
         return(
-         <div class="Container">
-             <h3>StartScreen Component</h3>
+         <div className="Container">
+             <div className="Logo">
+                <img src={require('../../CryptoSendLogo.png').default} alt="Logo"/>
+             </div>
+             <div className="TitleText">
+                 <p>Crypto address book</p>
+             </div>
+             <div className="DescriptionText">
+                 <p>The easiest and quickest way to manage and pay your contact. Connect your wallet to begin</p>
+             </div>
+             <div className="ConnectToWalletButton">
+                 <button>Connect Wallet</button>
+             </div>
          </div>
         )
 }
